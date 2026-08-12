61982bbe4a4878016942f0a38d5ffe21e59acc3d
cookieukw <cookieukw@users.noreply.github.com>
2026-08-12 20:42:53 -0300

    feat: import List interface in TimelineState.java

--- src/main/java/gg/alexandre/replay/replay/state/TimelineState.java
+++ src/main/java/gg/alexandre/replay/replay/state/TimelineState.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
